afde6be9feaf5b66dbe2586ec873ac3f9c1cf857
Felipe <felipeagostinis7@gmail.com>
2026-08-21 20:05:39 -0300

    criando o painel de adm do site

---
+++ admin/painel.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Painel Administrativos</title>
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="bg-light">
+
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container">
+            <span class="navbar-brand mb-0 h1">
+                Be Fit - Painel Administrativo
+            </span>
+
+            <a href="../index.php" class="btn btn-outline-light">
+                Ver Site
+            </a>
+        </div>
+    </nav>
+</body>
+</html>
