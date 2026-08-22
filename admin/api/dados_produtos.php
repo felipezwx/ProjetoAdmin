dc19b4d9d6cc64105be2b7f41b8ccfafe786bc2b
Felipe <felipeagostinis7@gmail.com>
2026-08-22 18:24:45 -0300

    api retornando em JSON

--- admin/api/dados_produtos.php
+++ admin/api/dados_produtos.php
@@ -2,8 +2,22 @@
 
 header('Content-Type: application/json; charset=utf-8');
 
-include '../conexao.php';
+include '../../conexao.php';
 
 $sql = "SELECT * FROM vw_produtos_categorias";
+
+$stmt = $conn->prepare($sql);
+
+$stmt->execute();
+
+$resultado = $stmt->get_result();
+
+$produtos = [];
+
+while ($produto = $resultado->fetch_assoc()) {
+    $produtos[] = $produto;
+}
+
+echo json_encode($produtos);
 
 ?>
