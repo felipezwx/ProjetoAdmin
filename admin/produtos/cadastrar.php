ff6af9544398af72e5bc18fd0137168bf6d2ac92
Felipe <felipeagostinis7@gmail.com>
2026-08-22 01:52:24 -0300

    começando a fazer a parte do visual de cadastrar

--- admin/produtos/cadastrar.php
+++ admin/produtos/cadastrar.php
@@ -57,3 +57,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         }
     }
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de Produtos</title>
+
+    <link rel="stylesheet" type="text/css" href="../../style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    
+</body>
+</html>
