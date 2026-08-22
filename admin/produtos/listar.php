af1105c3c218ed027285cdf576be617087741b43
Felipe <felipeagostinis7@gmail.com>
2026-08-21 21:48:34 -0300

    iniciando e adicionando botões de adicionar produto e voltar

--- admin/produtos/listar.php
+++ admin/produtos/listar.php
@@ -7,3 +7,33 @@ $sql = "SELECT * FROM produtos";
 $resultado = $conn->query($sql);
 
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"rel="stylesheet">
+</head>
+<body>
+    <div class="container mt-5">
+
+        <h1 class="mb-4">
+            Produtos
+        </h1>
+
+
+        <a href="../painel.php" class="btn btn-secondary mb-3">
+            Voltar
+        </a>
+
+
+        <a href="cadastrar.php" class="btn btn-success mb-3">
+            Novo Produto
+        </a>
+    </div>
+</body>
+</html>
