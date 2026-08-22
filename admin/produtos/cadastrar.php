2fe6507676ce653ce03926f978636223ff4ed14d
Felipe <felipeagostinis7@gmail.com>
2026-08-22 01:14:21 -0300

    fazendo a verificação se ja existe um produto com o nome no banco de dados

--- admin/produtos/cadastrar.php
+++ admin/produtos/cadastrar.php
@@ -5,3 +5,27 @@ include '../../conexao.php';
 $sqlCategorias = "SELECT * FROM categorias";
 
 $resultadoCategorias = $conn->query($sqlCategorias);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $nome = $_POST['nome'];
+    $descricao = $_POST['descricao'];
+    $preco = $_POST['preco'];
+    $estoque = $_POST['estoque'];
+    $imagem = $_POST['imagem'];
+    $id_categoria = $_POST['id_categoria'];
+
+    $sqlVerificar = "SELECT * FROM produtos WHERE nome = ?";
+
+    $stmtVerificar = $conn->prepare($sqlVerificar);
+
+    $stmtVerificar->bind_param("s", $nome);
+
+    $stmtVerificar->execute();
+
+    $resultadoVerificar = $stmtVerificar->get_result();
+
+
+    if ($resultadoVerificar->num_rows > 0) {
+
+        $mensagem = "Já existe um produto com esse nome.";
