08dfda06d6fd87c3bfbd541f2105d6e4781b9090
Felipe <felipeagostinis7@gmail.com>
2026-08-23 00:39:01 -0300

    adicionando featch da api para fazer o calculo de valor em estoque

--- admin/painel.js
+++ admin/painel.js
@@ -3,6 +3,10 @@ var carregarProdutos = async () => {
     try {
         const resposta = await fetch("api/dados_produtos.php");
         const produtos = await resposta.json();
+        const valorTotal = produtos.reduce((total, produto) => {
+            return total + (Number(produto.preco) * Number(produto.estoque));
+        }, 0);
+        console.log("Valor total do estoque:", valorTotal);
         console.log(produtos);
     }
     catch (erro) {
--- admin/painel.ts
+++ admin/painel.ts
@@ -6,6 +6,14 @@ var carregarProdutos = async () => {
 
         const produtos = await resposta.json();
 
+        const valorTotal = produtos.reduce((total: number, produto: { preco: string, estoque: string}) => {
+
+        return total + (Number(produto.preco) * Number(produto.estoque));
+
+        }, 0);
+
+console.log("Valor total do estoque:", valorTotal);
+
         console.log(produtos);
 
     } catch (erro) {
