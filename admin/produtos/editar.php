09b724913f5066f71f2db6d2d1c820b4e3ad1bb2
Felipe <felipeagostinis7@gmail.com>
2026-09-09 13:24:58 -0300

    começando a criar a pagina de editar produtos

--- admin/produtos/editar.php
+++ admin/produtos/editar.php
@@ -16,6 +16,30 @@ $resultado = $stmt->get_result();
 
 $produto = $resultado->fetch_assoc();
 
-echo $produto['nome'];
-
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Editar Produto</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../style-admin.css">
+</head>
+<body>
+    
+    <div class="container py-5">
+
+        <div class="card shadow">
+
+            <div class="card-header">
+                <h2>Editar Produto</h2>
+            </div>
+
+        </div>
+
+    </div>
+</body>
+</html>
